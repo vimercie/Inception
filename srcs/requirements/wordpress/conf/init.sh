a67eb081395e5b9a64c11dbc23587336d466959c
vimercie <vimercie@student.42lyon.fr>
2023-10-10 16:17:35 +0200

    added init.sh to wordpress

---
+++ srcs/requirements/wordpress/conf/init.sh
@@ -0,0 +1,5 @@
+sleep 10
+wp config create    --dbname=$SQL_DATABASE      \
+                    --dbuser=$SQL_USER          \
+                    --dbpass=$SQL_PASSWORD      \
+                    --dbhost=mariadb:3306 --path='/var/www/wordpress'
