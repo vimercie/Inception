16547e5dd82a328c97de83b965727a24caf2a208
vimercie <vimercie@student.42lyon.fr>
2023-10-27 20:29:52 +0200

    COMMIT 2

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,15 @@
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define( 'DB_NAME', '${SQL_DATABASE}' );
+
+/** Utilisateur de la base de données MySQL. */
+define( 'DB_USER', '${SQL_USER}' );
+
+/** Mot de passe de la base de données MySQL. */
+define( 'DB_PASSWORD', '${SQL_PASSWORD}' );
+
+/** Adresse de l hébergement MySQL. */
+define( 'DB_HOST', 'mariadb:3306' );
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define( 'DB_CHARSET', 'utf8' );
