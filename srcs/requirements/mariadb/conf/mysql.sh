b70fe3da22a53b10442a873d24773885ed23ae2f
vimercie <vincent.mrc@hotmail.com>
2023-10-29 02:22:27 +0100

    COMMIT 3

--- srcs/requirements/mariadb/conf/mysql.sh
+++ srcs/requirements/mariadb/conf/mysql.sh
@@ -1,7 +1,27 @@
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'${SQL_HOST}' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'${SQL_HOST}' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-service mysql stop;
+#Check if database is already created (in case of crash or pause, don't recreate it)
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]
+then
+	#Create socket for mysql (to run sql)
+	service mysql start
+	#sleep to make sure socket has been created
+	sleep 3
+	#mysql -e = run command without open mysql (like a subshell)
+	#Create database
+	mysql -e "CREATE DATABASE ${SQL_DATABASE}" && \
+	#Create a new user (the percent is use to be able to connect from anywhere)
+	mysql -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" && \
+	#Get him all privileges
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}' WITH GRANT OPTION;" && \
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}' WITH GRANT OPTION;" && \
+	#Make sure grants take effect right now
+	mysql -e "FLUSH PRIVILEGES;"
+	#Make the root password take effect's
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}'"
+	#Change root password
+	sed -i "s/password =/password = ${SQL_ROOT_PASSWORD} #/" /etc/mysql/debian.cnf
+	#stop mysql to be able to launch mysqld_safe
+	service mysql stop
+fi
+
+#start mysql server in safe mode (will restart if crash) and send data to path
+mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -3,4 +3,6 @@ wp config create	--allow-root \
 			        --dbname=$SQL_DATABASE \
 			        --dbuser=$SQL_USER \
 			        --dbpass=$SQL_PASSWORD \
-			        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+			        --dbhost=mariadb:3306 --path='/var/www/wordpress'RUN		wp core config --allow-root --path=/var/www/wordpress --dbname=wordpress --dbuser=mysql --dbpass=password --dbhost=mysql --dbprefix=wp_
+wp core install
+wp user create --allow-root --path=/var/www/wordpress admin
